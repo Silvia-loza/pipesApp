a6475187f271fd0c0ace8032ad846f051d23f1fb
Silvia Lozano <sylvilozano@gmail.com>
2024-01-22 00:09:01 +0100

    feature: added content for basics page, added primeflex library and set main styles

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
-import { MenuModule } from 'primeng/menu';
 import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ButtonModule, MenuModule, MenubarModule],
-  exports: [ButtonModule, MenuModule, MenubarModule],
+  imports: [],
+  exports: [
+    ButtonModule,
+    MenubarModule,
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    PanelModule,
+  ],
 })
 export class PrimeNgModule {}
--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basics-page',
   templateUrl: './basics-page.component.html',
-  styleUrl: './basics-page.component.css'
+  styleUrl: './basics-page.component.css',
 })
 export class BasicsPageComponent {
+  public nameLower: string = 'silvia';
+  public nameUpper: string = 'SILVIA';
+  public fullName: string = 'sIlviA LOzaNO';
 
+  public customDate: Date = new Date();
 }
