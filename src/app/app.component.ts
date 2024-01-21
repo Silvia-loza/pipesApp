83105ce13540b29b651e8485185f98821535d504
Silvia Lozano <sylvilozano@gmail.com>
2024-01-21 20:41:31 +0100

    feature: Created main architecture and added primeNg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem, PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'pipesApp';
+export class AppComponent implements OnInit {
+  title = 'silvia lozano';
+  items: MenuItem[] | undefined;
+
+  constructor(private primengConfig: PrimeNGConfig) {
+    this.primengConfig.ripple = true;
+  }
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'New',
+        icon: 'pi pi-fw pi-plus',
+      },
+      {
+        label: 'Delete',
+        icon: 'pi pi-fw pi-trash',
+      },
+    ];
+  }
 }
---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ButtonModule, MenuModule],
+  exports: [ButtonModule, MenuModule],
+})
+export class PrimeNgModule {}
