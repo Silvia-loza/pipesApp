3904778459c0317941d379e3df9c7295207325fe
Silvia Lozano <sylvilozano@gmail.com>
2024-01-22 00:29:27 +0100

    feature: Set up and examples of use for locales in pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+//set locales
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+import localeEn from '@angular/common/locales/en';
+
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs);
+registerLocaleData(localeFr);
+registerLocaleData(localeEn);
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +25,13 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
   ],
   exports: [],
-  providers: [],
+  //Sets the locale to use for the application as default
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-ES',
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
