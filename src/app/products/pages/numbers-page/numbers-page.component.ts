13e48d34c9d6994ce157323203bbad26e63123b2
Silvia Lozano <sylvilozano@gmail.com>
2024-01-22 16:46:20 +0100

    feature: added content to pipe numbers page

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers-page',
   templateUrl: './numbers-page.component.html',
-  styleUrl: './numbers-page.component.css'
+  styleUrl: './numbers-page.component.css',
 })
 export class NumbersPageComponent {
-
+  public totalSales: number = 2555465.5284;
+  public percentages: number = 0.4561598;
 }
