feca987140b69f0a203744c22d2a1566083fb1c8
Silvia Lozano <sylvilozano@gmail.com>
2024-01-23 11:40:36 +0100

    feature: Added p-table for hero display in order page

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -6,6 +6,7 @@ import { FieldsetModule } from 'primeng/fieldset';
 import { PanelModule } from 'primeng/panel';
 import { ToolbarModule } from 'primeng/toolbar';
 import { SplitButtonModule } from 'primeng/splitbutton';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [],
@@ -20,6 +21,7 @@ import { SplitButtonModule } from 'primeng/splitbutton';
     FieldsetModule,
     ToolbarModule,
     SplitButtonModule,
+    TableModule,
   ],
 })
 export class PrimeNgModule {}
--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Color, Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'products-order',
   templateUrl: './order.component.html',
   styles: ``,
 })
-export class OrderComponent {
-  public items: any[] = [];
+export class OrderComponent implements OnInit {
+  public isUpperCase: boolean = false;
+  public heros: Hero[] = [
+    { name: 'superman', canfly: true, color: Color.blue },
+    {
+      name: 'batman',
+      canfly: false,
+      color: Color.black,
+    },
+    {
+      name: 'ironman',
+      canfly: true,
+      color: Color.red,
+    },
+    {
+      name: 'spiderman',
+      canfly: false,
+      color: Color.green,
+    },
+  ];
 
   constructor() {}
+  ngOnInit(): void {
+    console.log(typeof this.heros[0].color);
+  }
+
+  toggleCase(): void {
+    this.isUpperCase = !this.isUpperCase;
+  }
 }
